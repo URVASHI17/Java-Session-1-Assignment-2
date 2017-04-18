cb78d554218e70cab635e0625dfe67ee6c15c725
URVASHI PANDYA <urvashichi87@gmail.com>
2017-04-18 18:43:59 +0530

    Add files via upload

---
+++ Integar.java
@@ -0,0 +1,16 @@
+class Integar
+	{
+		public static void main(String args[])
+	{
+		int a=20,b=10,c,d,e;
+		b=a-- - --a;
+		c=a--;
+		d=a>>2;
+		e=a&b;
+		
+		System.out.println("the first answer is"+b);
+		System.out.println("the second answer is"+c);
+		System.out.println("the third answer is"+d);
+		System.out.println("the fourth answer is"+e);
+	}
+	}
